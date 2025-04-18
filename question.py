c32924b5d4b872c8ee12474d20f60765584d1edf
KapiCraft <zoltan.kapeller@gmail.com>
2025-04-18 23:23:25 +0200

    evaluating sets added

--- question.py
+++ question.py
@@ -12,8 +12,8 @@ class Question:
         self.question = question
         self._answer = None
         self.correct_answer = answer
-        self.max_point = points
-        self.precision = precision
+        self.precision = precision if precision else Precision.NORMAL
+        self.max_point = int(points if points else 10)
         self.point = 0
 
     def __repr__(self):
@@ -27,28 +27,25 @@ class Question:
             case QuestionType.DATE: self._evaluate_date()
             case QuestionType.STRING: self._evaluate_string()
             case QuestionType.SET: self._evaluate_set()
-            case _: self.point = self.max_point
+            case _: raise NotImplementedError(f"Unknown question type: {self.type}")
 
     def _evaluate_string(self):
-        self._answer = self._answer.lower()
-        self.correct_answer = self.correct_answer.lower()
-        accuracy = 1 - SequenceMatcher(None, self._answer, self.correct_answer).ratio()
-        self._calculate_pts(accuracy)
+        accuracy = 1 - SequenceMatcher(None, self._answer.lower(), self.correct_answer.lower()).ratio()
+        self._calculate_points(accuracy)
 
     def _evaluate_date(self):
-        self._answer = date.fromisoformat(self._answer)
-        self.correct_answer = date.fromisoformat(self.correct_answer)
-        days = abs((self._answer - self.correct_answer).days)
+        d1 = date.fromisoformat(self._answer)
+        d2 = date.fromisoformat(self.correct_answer)
+        days = abs((d1 - d2).days)
         accuracy = 0.05 if days <= 30 else 0.1 if days <= 180 else 0.2 if days <= 365 else 1
-        self._calculate_pts(accuracy)
+        self._calculate_points(accuracy)
 
     def _evaluate_number(self):
-        self._answer = float(self._answer)
-        difference = abs(float(self.correct_answer) - self._answer)
+        difference = abs(float(self.correct_answer) - float(self._answer))
         accuracy = difference / self.correct_answer
-        self._calculate_pts(accuracy)
+        self._calculate_points(accuracy)
 
-    def _calculate_pts(self, accuracy):
+    def _calculate_points(self, accuracy):
         pts = [self.max_point, int(self.max_point * 0.8), 0]
         match self.precision:
             case Precision.LOW: pts_range = [0.2, 0.4]
@@ -59,15 +56,12 @@ class Question:
     def _evaluate_set(self):
         self._answer = self._answer.strip().lower()
         self._answer = self._answer.split(",")
-        self._answer = list(map(lambda x: x.replace("  ", " "), self._answer))
-        self._answer = list(map(lambda x: x.lstrip().rstrip(), self._answer))
-        self.correct_answer = [x.lower() for x in self.correct_answer]
+        self._answer = list(map(lambda x: x.replace("  ", " ").lstrip().rstrip(), self._answer))
         self._calculate_set_points()
 
-
     def _calculate_set_points(self):
-        good_answer = [x for x in self.correct_answer if x in self._answer]
-        self.point = round((len(good_answer) / len(self.correct_answer)) * self.max_point)
+        good_answers = [x for x in self.correct_answer if x.lower() in self._answer]
+        self.point = round((len(good_answers) / len(self.correct_answer)) * self.max_point)
 
     @property
     def answer_was_not_correct(self):
